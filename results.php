7bbf288e99dd5c36aaf63189a745575a4f204835
nathantschultz <nathantschultz@gmail.com>
2014-01-21 10:26:12 -0700

    form

---
+++ results.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Results Page</title>
+		
+	</head>
+	<body>
+
+		<?php
+		echo $_POST["name"];
+		
+		
+		
+		
+		
+		
+		?>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+<!--
+		<h1>Our form</h1>
+		
+		<form action="results.php" method="post">
+		Name:<input type="text" name="name">
+		Email:<input type="text" name="email"> 
+		Major:
+		<input type="radio" name="major" value="Computer Science">Computer Science<br/>
+		<input type="radio" name="major" value="Web Development and Design">Web Development and Design<br/>
+		<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br/>
+		<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br/>
+		<br>
+		Places I have visited:
+		<input type="checkbox" name="places" value="North America">North America<br/>
+		<input type="checkbox" name="places" value="South America">South America<br/>
+		<input type="checkbox" name="places" value="Europe">Europe<br/>
+		<input type="checkbox" name="places" value="Asia">Asia<br/>
+		<input type="checkbox" name="places" value="Australia">Australia<br/>
+		<input type="checkbox" name="places" value="Africa">Africa<br/>
+		<input type="checkbox" name="places" value="Antarctica">Antarctica<br/>
+		<br>
+		Any other comments?
+		<input type="textarea" name="comments"><br/>
+		<input type="submit" value="Submit">
+		</form>
+-->
+
+
+
+	</body>
+</html>
