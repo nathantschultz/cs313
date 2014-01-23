e82a9f2a8595f6d450aacaed9cc71e32a04a0118
nathantschultz <nathantschultz@gmail.com>
2014-01-23 14:48:48 -0700

    survey

--- survey.php
+++ survey.php
@@ -6,25 +6,25 @@
 	<body>
 		<h1>Survey</h1>	
 		<form action="results2.php" method="post">
-		1. What is your favorite ice cream?	
+		1. What is your favorite ice cream?<br />	
 		<input type="radio" name="ice" value="Chocolate">Chocolate<br/>
 		<input type="radio" name="ice" value="Vanilla">Vanilla<br/>
 		<input type="radio" name="ice" value="Mint Chocolate Chip">Mint Chocolate Chip<br/>
 		<input type="radio" name="ice" value="Sherbet">Sherbet<br/>
 		<br>
-		2. What is your favorite operating system?
+		2. What is your favorite operating system?<br />
 		<input type="radio" name="os" value="Mac">Mac<br/>
 		<input type="radio" name="os" value="Windows">Windows<br/>
 		<input type="radio" name="os" value="Linux">Linux<br/>
 		<input type="radio" name="os" value="DOS">DOS<br/>
 		<br>
-		3. What is your favorite mobile operating system?
+		3. What is your favorite mobile operating system?<br />
 		<input type="radio" name="phone" value="iOS">iOS<br/>
 		<input type="radio" name="phone" value="Windows Phone 8">Windows Phone 8<br/>
 		<input type="radio" name="phone" value="Android">Android<br/>
 		<input type="radio" name="phone" value="WebOS">WebOS<br/>
 		<br>
-		4. What is your favorite browser?
+		4. What is your favorite browser?<br />
 		<input type="radio" name="browser" value="Internet Explorer">Internet Explorer<br/>
 		<input type="radio" name="browser" value="Safari">Safari<br/>
 		<input type="radio" name="browser" value="Chrome">Chrome<br/>
