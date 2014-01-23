d2d709de218a95ed91e33977871e30b1c21e1980
nathantschultz <nathantschultz@gmail.com>
2014-01-23 14:46:42 -0700

    survey

---
+++ file.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Results Page</title>
+		
+	</head>
+	<body>
+		<h1>File Page</h1>
+	<?php
+	
+		//one string
+		if ($count = file_get_contents("file.txt)"){
+			echo $count;
+		}
+	
+	
+		//array of lines
+		if ($contents = file("file.txt"){
+			foreach ($contents as $value){
+				echo "$value <br/>"
+			}
+		}
+	
+		//another way
+		$file = fopen("file.txt", "r+");
+		
+		if ($file){
+			while (!feof($file)){
+				$line = fgets($file);
+				print("$line <br/>");
+			}
+		}
+	
+	
+		fwrite($file, "New Content");
+	
+	
+		fclose($file);
+	
+	?>
+		
+		
+		
+	</body>
+</html>
+		
+		
---
+++ survey.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Survey</title>
+	</head>
+	<body>
+		<h1>Survey</h1>	
+		<form action="results2.php" method="post">
+		1. What is your favorite ice cream?	
+		<input type="radio" name="ice" value="Chocolate">Chocolate<br/>
+		<input type="radio" name="ice" value="Vanilla">Vanilla<br/>
+		<input type="radio" name="ice" value="Mint Chocolate Chip">Mint Chocolate Chip<br/>
+		<input type="radio" name="ice" value="Sherbet">Sherbet<br/>
+		<br>
+		2. What is your favorite operating system?
+		<input type="radio" name="os" value="Mac">Mac<br/>
+		<input type="radio" name="os" value="Windows">Windows<br/>
+		<input type="radio" name="os" value="Linux">Linux<br/>
+		<input type="radio" name="os" value="DOS">DOS<br/>
+		<br>
+		3. What is your favorite mobile operating system?
+		<input type="radio" name="phone" value="iOS">iOS<br/>
+		<input type="radio" name="phone" value="Windows Phone 8">Windows Phone 8<br/>
+		<input type="radio" name="phone" value="Android">Android<br/>
+		<input type="radio" name="phone" value="WebOS">WebOS<br/>
+		<br>
+		4. What is your favorite browser?
+		<input type="radio" name="browser" value="Internet Explorer">Internet Explorer<br/>
+		<input type="radio" name="browser" value="Safari">Safari<br/>
+		<input type="radio" name="browser" value="Chrome">Chrome<br/>
+		<input type="radio" name="browser" value="Firefox">Firefox<br/>
+		<br>
+		<input type="submit" value="Submit">
+		</form>
+	</body>
+</html>
